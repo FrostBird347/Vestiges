6c9f5a1be22ce6525d6cf2e81a5126d5e0a63717
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2023-02-22 19:15:00 +0800

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using System.Security.Permissions;
+using System;
+
+// Allows access to private members
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace RENAME_ME {
+
+	[BepInPlugin("frostbird347.renameme", "REMAME_ME", "0.1.0")]
+	public sealed class Plugin : BaseUnityPlugin {
+		bool init;
+		private PluginOptions Options = null;
+		bool configWorking = false;
+
+		public void OnEnable() {
+			// Add hooks here
+			On.RainWorld.OnModsInit += Init;
+		}
+
+		private void Init(On.RainWorld.orig_OnModsInit orig, RainWorld self) {
+			orig(self);
+
+			if (!init) {
+				init = true;
+
+				try {
+					Options = new PluginOptions(this, Logger);
+					MachineConnector.SetRegisteredOI("frostbird347.hairlesssaint", Options);
+					configWorking = true;
+				} catch (Exception err) {
+					Logger.LogError(err);
+					configWorking = false;
+				}
+			}
+		}
+	}
+}
---
+++ src/PluginOptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Policy;
+using BepInEx.Logging;
+using Menu.Remix.MixedUI;
+using UnityEngine;
+
+namespace RENAME_ME {
+
+	public class PluginOptions : OptionInterface {
+		private readonly ManualLogSource Logger;
+
+		public readonly Configurable<float> exampleFloat;
+		public readonly Configurable<bool> exampleBool = new Configurable<bool>(false);
+		private UIelement[] UIArrPlayerOptions;
+
+		public PluginOptions(Plugin pluginInstance, ManualLogSource logSource) {
+			Logger = logSource;
+			exampleFloat = config.Bind("exampleFloat", 1f, new ConfigAcceptableRange<float>(0f, 100f));
+			exampleBool = config.Bind("exampleBool", false, (ConfigurableInfo)null);
+		}
+
+		public override void Initialize() {
+			OpTab opTab = new OpTab(this, "Options");
+			this.Tabs = new[]
+			{
+				opTab
+			};
+
+			UIArrPlayerOptions = new UIelement[]
+			{
+				new OpLabel(10f, 550f, "Options", true),
+				new OpLabel(10f, 520f, "Example float"),
+				new OpUpdown(exampleFloat, new Vector2(150f,520f), 100f, 1),
+				new OpLabel(10f, 490f, "Example bool"),
+				new OpCheckBox(exampleBool, new Vector2(200f,490f)) { description = "Hover description" }
+			};
+			opTab.AddItems(UIArrPlayerOptions);
+		}
+	}
+}
