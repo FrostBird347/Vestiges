0c01f62398c480c48380b69627ce64b7e12ec719
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2023-03-06 22:16:39 +0800

    Set version to 0.10.1

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -21,7 +21,7 @@ using System.Threading.Tasks;
 
 namespace Vestiges {
 
-	[BepInPlugin("frostbird347.vestiges", "Vestiges", "0.10.0")]
+	[BepInPlugin("frostbird347.vestiges", "Vestiges", "0.10.1")]
 	public sealed class Plugin : BaseUnityPlugin {
 		bool init;
 		private PluginOptions Options = null;
