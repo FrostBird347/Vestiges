247fee783810380e93874d927d4d5a167feee867
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2026-02-02 13:29:11 +0800

    Also clear previous builds

--- PostBuild.sh
+++ PostBuild.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+projectName="$1"
 versionId="$(cat src/Plugin.cs | grep 'public const string PLUGIN_VERSION = ' | cut -d\" -f2)"
 newJSON="$(cat ./assets/modinfo.json | jq ".version = \"${versionId}\"")"
 if [[ "$newJSON" != "" && versionId != "" ]]
 then
 	printf "$newJSON" > ./assets/modinfo.json
+fi
+
+if [[ "$projectName" != "" && -d "../_resources/Rain World/RainWorld_Data/StreamingAssets/mods/${projectName}/" ]]
+then
+	rm -r "../_resources/Rain World/RainWorld_Data/StreamingAssets/mods/${projectName}/"
 fi
